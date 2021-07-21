fc2e340617c40248f35f7a96758823d9aed617c6
youchuyu <1396626729@qq.com>
2021-07-21 17:48:55 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,20 @@
 <template>
   <div id="app">
+    <nav-bar></nav-bar>
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
     </div>
     <router-view/>
   </div>
 </template>
+<script>
+import navBar from '@/components/commonComponent/navBar/navBar.vue'
+export default {
+  name:'app',
+  components:{
+    navBar
+  }
+}
+</script>
 
 <style lang="less">
 #app {
--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,7 @@ Vue.use(VueRouter)
 const routes = [
   {
     path: '/',
+    redirect:'/allpages',
     name: 'Home',
     component: Home
   },
@@ -17,6 +18,13 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },{
+    path: '/allpages',
+    name: 'Allpages',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/dataVisualPages/ParallelsPage.vue')
   }
 ]
 
